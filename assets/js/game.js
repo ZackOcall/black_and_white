5eea730f263090eb1757d8a8c3d704d1a70d1c65
Zack O'Callaghan <zocallaghan@googlemail.com>
2022-03-01 21:31:45 +0000

    add playerCard object

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,5 +16,16 @@ let computerCard = [
     { div: "7", id: 7, name: "computer 7" },
     { div: "8", id: 8, name: "computer 8" },
   ];
+let playerCard = [
+    { div: "0", id: 9, name: "player 0" },
+    { div: "1", id: 10, name: "player 1" },
+    { div: "2", id: 11, name: "player 2" },
+    { div: "3", id: 12, name: "player 3" },
+    { div: "4", id: 13, name: "player 4" },
+    { div: "5", id: 14, name: "player 5" },
+    { div: "6", id: 15, name: "player 6" },
+    { div: "7", id: 16, name: "player 7" },
+    { div: "8", id: 17, name: "player 8" },
+  ];
 }
 const board
